1207af3619403c8cad82d87ee15d37aa46897785
lambor <1973124271@qq.com>
2017-06-13 15:25:50 +0800

    tmp commit

---
+++ microblog/routes/index.js
@@ -0,0 +1,40 @@
+
+/*
+ * microblog routes.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.user = function(req,res) {
+
+};
+
+exports.post = function(req,res) {
+
+};
+
+exports.reg = function(req,res) {
+
+};
+
+exports.doReg = function(req,res) {
+
+};
+
+exports.login = function(req,res) {
+
+};
+
+exports.doLogin = function(req,res) {
+
+};
+
+exports.user = function(req,res) {
+
+};
+
+exports.logout = function(req,res) {
+
+}
